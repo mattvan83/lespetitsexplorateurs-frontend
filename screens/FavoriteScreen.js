7f26bf2b47c458c1fbc119c19e7de1f29556c9fd
Your name <Your email>
2024-03-04 16:16:40 +0100

    Fiches Front

--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -8,9 +8,20 @@ import {
 import globalStyles from '../globalStyles';
 
 export default function FavoriteScreen({ navigation }) {
+  const handleSubmit = () => {
+    navigation.navigate("ActivitySheet");
+  };
+
   return (
     <View style={styles.container}>
       <Text style={globalStyles.title2}>Mes favoris</Text>
+      <TouchableOpacity
+        onPress={() => handleSubmit()}
+        style={styles.button}
+        activeOpacity={0.8}
+      >
+        <Text style={styles.textButton}>Go to ActivitySheet</Text>
+      </TouchableOpacity>
     </View>
   );
 }
