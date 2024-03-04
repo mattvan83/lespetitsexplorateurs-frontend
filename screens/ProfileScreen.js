29d549b83bd461d8706255adfd57e82eb4f4af88
Emeline <emeline.courcelle@gmail.com>
2024-03-04 11:59:49 +0100

    Add logout

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -5,11 +5,27 @@ import {
   View,
   TouchableOpacity,
 } from "react-native";
+import globalStyles from '../globalStyles';
+import { useDispatch } from 'react-redux';
+import { logout } from '../reducers/user';
 
 export default function ProfileScreen({ navigation }) {
+  const dispatch = useDispatch();
+
+  const handleLogOut = () => {
+    dispatch(logout())
+    navigation.navigate('Explorer')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Profile Screen</Text>
+      <Text style={globalStyles.title2}>Mon profil</Text>
+      <TouchableOpacity
+          onPress={() => handleLogOut()}
+          activeOpacity={0.8}
+        >
+          <Text style={styles.deconnexion}>Se déconnecter</Text>
+        </TouchableOpacity>
     </View>
   );
 }
@@ -17,8 +33,10 @@ export default function ProfileScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "green",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "white",
   },
+  deconnexion : {
+    margin : 20,
+
+  }
 });
