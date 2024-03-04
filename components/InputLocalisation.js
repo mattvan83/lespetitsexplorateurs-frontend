7201edb6b9a211949abeccc0c0921e5440ff1f0a
Your name <Your email>
2024-03-04 17:21:56 +0100

    fix conflicts

--- components/InputLocalisation.js
+++ components/InputLocalisation.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import {
   StyleSheet,
   TextInput,
@@ -8,32 +7,9 @@ import {
   TouchableOpacity,
 } from "react-native";
 import SvgUri from "react-native-svg-uri";
-=======
-import { StyleSheet, TextInput, Text, Image, View, TouchableOpacity } from 'react-native';
-// import SvgUri from 'react-native-svg-uri';
->>>>>>> signupscreen
 
 export default function InputLocalisation() {
   const handleSubmit = () => {};
-
-<<<<<<< HEAD
-  return (
-    <View style={styles.searchContainer}>
-      <View style={styles.search}>
-        <View style={styles.searchBar}>
-          <SvgUri
-            width="24"
-            height="24"
-            source={require("../assets/icons/filter-pin.svg")}
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Saisissez une ville..."
-          />
-=======
-    const handleSubmit = () => {
-
-    }
 
     return (
         <View style={styles.searchContainer}>
@@ -52,10 +28,7 @@ export default function InputLocalisation() {
                 >
                      {/* <SvgUri width="28" height="28" source={require('../assets/icones/filter-geoloc.svg')} /> */}
                 </TouchableOpacity>
-
             </View>
->>>>>>> signupscreen
-        </View>
         <TouchableOpacity
           onPress={() => handleSubmit()}
           style={styles.geolocButton}
@@ -68,47 +41,10 @@ export default function InputLocalisation() {
           />
         </TouchableOpacity>
       </View>
-    </View>
   );
 }
 
 const styles = StyleSheet.create({
-<<<<<<< HEAD
-  searchContainer: {
-    alignItems: "center",
-  },
-  search: {
-    flexDirection: "row",
-    gap: 8,
-    height: 40,
-    width: "90%",
-  },
-  searchBar: {
-    flex: 1,
-    alignItems: "center",
-    flexDirection: "row",
-    paddingLeft: 10,
-    borderRadius: 12,
-    borderColor: "#E4dfdf",
-    borderWidth: 1,
-  },
-  input: {
-    width: "90%",
-    color: "#747688",
-    fontSize: 16,
-    marginLeft: 10,
-  },
-  geolocButton: {
-    width: 40,
-    height: 40,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#5669FF",
-    borderRadius: 8,
-    padding: 4,
-  },
-});
-=======
     searchContainer: {
         alignItems: 'center',
         marginTop: 20,
@@ -144,4 +80,3 @@ const styles = StyleSheet.create({
         padding: 4,
     },
 });
->>>>>>> signupscreen
